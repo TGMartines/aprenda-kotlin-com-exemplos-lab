69be741b85bb364d7103cdb296a5e98a066e0f23
Thiago Martines <142542192+TGMartines@users.noreply.github.com>
2023-08-28 15:32:21 -0400

    Código melhorado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,38 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    val matriculados = mutableListOf<Usuario>()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        matriculados.add(usuario)
+        println("Usuário ${usuario.hashCode()} matriculado na formação $nome")
+    }
+
+    fun exibirMatriculados() {
+        println("Alunos matriculados na formação $nome: ")
+        matriculados.forEach { aluno ->
+            println("Aluno: ${aluno.nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Lógica de Programação", 50)
+    val conteudo2 = ConteudoEducacional("Introdução ao Kotlin", 70)
+
+    val formacaoInKotlin = Formacao("Iniciando em Desenvolvimento com Kotlin", Nivel.BASICO, listOf(conteudo1, conteudo2))
+
+    val usuario1 = Usuario("Thiago")
+    val usuario2 = Usuario("Pedro")
+
+    listOf(formacaoInKotlin.matricular(usuario1), formacaoInKotlin.matricular(usuario2))
+
+    formacaoInKotlin.exibirMatriculados()
 }
